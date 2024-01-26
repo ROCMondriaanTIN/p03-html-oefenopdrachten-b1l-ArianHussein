1746f07a966adc8826566d44fd1d718f489e9ad7
ArianHussein <302453552@student.rocmondriaan.nl>
2024-01-26 10:30:02 +0100

    html les 1 aangepast

--- js/main.js
+++ js/main.js
@@ -5,7 +5,43 @@ const animes = [
   { title: "Blue Lock", characters: "Nagi" },
 ];
 
+
+const projects = [
+    { title: "hoger lager",
+     description: "dit is mijn project",
+      img:"img/sukuna.jpg"
+    },
+
+    { title: "Escape Room",
+     characters: "Project escape room",
+      img:"img/kaneki.jpg"
+     },
+];
+
+function createProject(title, description, img) {
+    let projectCard = `
+    <div class="card">
+    <h3>${title}</h3>
+    <p>${description}</p>
+    <img src="${img}" alt="">
+</div>
+      `;
+  
+    return projectCard;
+  }
+
+
 const card = document.querySelector(".card");
+
+const projects1 = document.querySelector(".projects-container");
+
+for (let i = 0; i < projects.length; i++) {
+    const project = projects[i];
+  
+    projects1.innerHTML += createProject(project.title, project.characters, project.img);
+  }
+
+
 
 function createcardWithpurplecolor(title, characters) {
   let cardHTML = `
